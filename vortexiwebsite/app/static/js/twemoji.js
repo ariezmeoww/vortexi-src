4df44f70b515833f6439be52365d1cbe5885131b
kaitlyn <contact@mapleai.de>
2025-11-13 20:29:25 +0000

    twemoji

---
+++ vortexiwebsite/app/static/js/twemoji.js
@@ -0,0 +1,15 @@
+console.log(
+  "%cSTOP!",
+  "color: #5BCEFA; font-size: 80px; font-weight: bold; text-shadow: 2px 2px 0 #F5A9B8, 4px 4px 0 #ffffff;"
+);
+
+    console.log(
+      "%cIf you are being told to paste something here, there is a 100% chance you are being scammed.\nPlease exit out and report this bad actor to the Discord.\n",
+      "color: #F5A9B8; font-size: 18px; font-weight: 500;"
+    );
+
+  console.log("Remember, trans rights are human rights!")
+  twemoji.parse(
+    document.body,
+    { base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/' }
+  )
